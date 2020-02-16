7166fe2a1319bba7c6f78d264500e567ef09c4d0
Danny Hollman <dannyhollman@gmail.com>
2020-02-16 21:51:56 +0000

    [INIT] Add files for task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,5 +1,17 @@
 $(document).ready(() => {
-  $('.am_checkbox').click(() => {
-    console.log($(;
+  let dict = {};
+  $('input[type="checkbox"]').change(function () {
+    let name = $(this).attr('data-name');
+    let id = $(this).attr('data-id');
+    if (this.checked) {
+      dict[name] = id;
+    } else {
+      delete dict[name];
+    }
+    if (Object.keys(dict).length > 0) {
+      $('#am_h4').text(Object.keys(dict).join(', '));
+    } else {
+      $('#am_h4').html('&nbsp;');
+    }
   });
 });
