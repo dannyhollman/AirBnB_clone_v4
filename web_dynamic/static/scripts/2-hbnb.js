aa494acfc2f190770d9c025a4aced0388d1318a7
Danny Hollman <dannyhollman@gmail.com>
2020-02-18 02:51:42 +0000

    [UPDATE] Fix css and update html files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,14 +1,28 @@
 $(document).ready(() => {
-  // const $statusId = $('DIV#api_status');
-  // const $addClass = 'available';
+  const dict = {};
+  $('input[type="checkbox"]').change(function () {
+    const name = $(this).attr('data-name');
+    const id = $(this).attr('data-id');
+    if (this.checked) {
+      dict[name] = id;
+    } else {
+      delete dict[name];
+    }
+    if (Object.keys(dict).length > 0) {
+      $('#am_h4').text(Object.keys(dict).join(', '));
+    } else {
+      $('#am_h4').html('&nbsp;');
+    }
+  });
+
   $.ajax({
     type: 'GET',
     url: 'http://0.0.0.0:5001/api/v1/status/',
     success: function (res) {
       if (res.status === 'OK') {
-        $('DIV#api_status').addClass('available');
+        $('#api_status').addClass('available');
       } else {
-        $('DIV#api_status').removeClass('available');
+        $('#api_status').removeClass('available');
       }
     }
   });
